9299805d8a26c0a026f8f2046d5de9df4e22f759
Oğuz Toraman <oguz.toraman@protonmail.com>
2022-02-26 00:10:46 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# A bash script to install requirement python3 libraries to fedora linux
-echo "opencv, numpy and scipy are installing..."
-sleep 1
-sudo dnf install python3-opencv python3-numpy python3-scipy
-echo "done!"
-exit
